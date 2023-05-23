3e3a800c9940fb1c8fbe93a943d7144865208bc7
Zakaria Ben <75684862+zaka-rgb@users.noreply.github.com>
2023-05-23 23:08:50 +0200

    Add files via upload

---
+++ boucle while/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int nombre=0;
+    printf("entrez le nombre 20\n");
+    scanf("%d",&nombre);
+    printf("\n\n");
+
+    while(nombre!=20)
+    {
+        printf("error error error \n\n");
+        printf("entrez SVP le nombre vingt \n\n");
+        scanf("%d",&nombre);
+    }
+    printf("bravooo");
+}
